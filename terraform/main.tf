cc2d1b1b7999031ac2885341adf719e191d67b5b
Ramy Maarouf <ramymaarouf255@gmail.com>
2026-04-21 14:29:43 -0400

    fix: merged team updates with new subscription RG fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,62 @@
-
-# Network
-module "networking" {
-  source = "./modules/networking"
-
-  resource_group_name = var.resource_group_name
-  location            = var.region
-  vnet_name           = var.vnet_name
-  address_space       = var.address_space
+# --- 1. TERRAFORM & PROVIDER CONFIG ---
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  # BACKEND IS COMMENTED OUT TO USE LOCAL STATE FOR NEW SUBSCRIPTION
+  /*
+  backend "azurerm" {
+    resource_group_name  = "cst8918-final-project-rg"
+    storage_account_name = "finalprojectst"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+  */
 }
 
-# 2. Member C's AKS Module
+provider "azurerm" {
+  features {}
+}
+
+# --- 2. RESOURCE GROUP ---
+# We create this here because it doesn't exist in your new subscription yet.
+resource "azurerm_resource_group" "project_rg" {
+  name     = "cst8918-final-project-group-2"
+  location = "canadacentral"
+}
+
+# --- 3. NETWORKING ---
+module "networking" {
+  source              = "./modules/networking"
+  resource_group_name = azurerm_resource_group.project_rg.name
+  location            = azurerm_resource_group.project_rg.location
+  vnet_name           = "final-project-vnet"
+  address_space       = ["10.0.0.0/14"]
+}
+
+# --- 4. CONTAINER REGISTRY (ACR) ---
 resource "azurerm_container_registry" "acr" {
-  name                = "cst8918group2acr"
-  resource_group_name = var.resource_group_name
-  location            = var.region
+
   sku                 = "Basic"
   admin_enabled       = true
 }
 
-# Test environment resources
+# --- 5. TEST RESOURCES ---
 module "test_resources" {
   source    = "./modules/test_resources"
-  location  = var.region
-  rg_name   = var.resource_group_name
+  rg_name   = azurerm_resource_group.project_rg.name
+  location  = azurerm_resource_group.project_rg.location
   subnet_id = module.networking.test_subnet_id
 }
 
-# Prod environment resources
+# --- 6. PROD RESOURCES ---
 module "prod_resources" {
   source    = "./modules/prod_resources"
-  location  = var.region
-  rg_name   = var.resource_group_name
+  rg_name   = azurerm_resource_group.project_rg.name
+  location  = azurerm_resource_group.project_rg.location
   subnet_id = module.networking.prod_subnet_id
 }
 
